efe143dbca31a8558413ee586135f166965276cf
GoldenDog435061 <78063969+GoldenDog1218@users.noreply.github.com>
2023-07-21 16:33:04 +0800

    Update and rename sidePC.lua to sidePC2.lua

--- sidePC2.lua
+++ sidePC2.lua
@@ -6,11 +6,24 @@ function createInterface()
     -- 清空屏幕
     monitor.clear()
 
+    -- 计算按钮位置
+    local buttonWidth = 6
+    local buttonHeight = 3
+    
+    local centerX = math.floor(screenWidth / 2)
+    local centerY = math.floor(screenHeight / 2)
+    
+    local changeButtonX = centerX - math.floor(buttonWidth / 2)
+    local changeButtonY = centerY - buttonHeight - 1
+    
+    local exitButtonX = centerX - math.floor(buttonWidth / 2)
+    local exitButtonY = centerY + 1
+
     -- 绘制按钮
-    monitor.setCursorPos(1, 1)
+    monitor.setCursorPos(changeButtonX, changeButtonY)
     monitor.write("change")
 
-    monitor.setCursorPos(1, 2)
+    monitor.setCursorPos(exitButtonX, exitButtonY)
     monitor.write("exit")
 end
 
@@ -18,11 +31,11 @@ end
 function listenTouchEvents()
     while true do
         local event, side, xPos, yPos = os.pullEvent("monitor_touch")
-        if xPos >= 1 and xPos <= 2 then
-            if yPos == 1 then
+        if xPos >= changeButtonX and xPos <= (changeButtonX + buttonWidth - 1) then
+            if yPos == changeButtonY then
                 -- 执行 change 按钮的操作
                 changeButtonAction()
-            elseif yPos == 2 then
+            elseif yPos == exitButtonY then
                 -- 执行 exit 按钮的操作
                 exitButtonAction()
             end
@@ -33,13 +46,13 @@ end
 -- change 按钮的操作
 function changeButtonAction()
     -- 在这里编写 change 按钮的操作代码
-    print("Change按钮被点击了！")
+    print("Change!")
 end
 
 -- exit 按钮的操作
 function exitButtonAction()
     -- 在这里编写 exit 按钮的操作代码
-    print("Exit按钮被点击了！")
+    print("Exit!")
     return
 end
 
