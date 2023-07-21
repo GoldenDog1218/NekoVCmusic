ccd5f0ca017817818ee66f3a1f994d3582910c56
GoldenDog435061 <78063969+GoldenDog1218@users.noreply.github.com>
2023-07-21 16:49:07 +0800

    Update sidePC2.lua

--- sidePC2.lua
+++ sidePC2.lua
@@ -1,18 +1,19 @@
-local buttonWidth = 2
-local buttonHeight = 1
+local buttonWidth = 8
+local buttonHeight = 3
 local screenWidth, screenHeight = term.getSize()
+local monitor = peripheral.find("monitor")
 
 -- 计算按钮位置
 local buttonY = math.floor(screenHeight / 2 - buttonHeight / 2)
-local changeButtonX = 2
-local exitButtonX = screenWidth - buttonWidth - 1
+local changeButtonX = math.floor(screenWidth / 2 - buttonWidth / 2)
+local exitButtonX = changeButtonX + buttonWidth + 2
 
--- 清空屏幕并绘制按钮
-term.clear()
-term.setCursorPos(changeButtonX, buttonY)
-print("Change")
-term.setCursorPos(exitButtonX, buttonY)
-print("Exit")
+-- 清空显示屏并绘制按钮
+monitor.clear()
+monitor.setCursorPos(changeButtonX, buttonY)
+monitor.write("[ Change ]")
+monitor.setCursorPos(exitButtonX, buttonY)
+monitor.write("[ Exit ]")
 
 -- 监听触摸事件
 while true do
@@ -22,11 +23,23 @@ while true do
     if yPos == buttonY and xPos >= changeButtonX and xPos < changeButtonX + buttonWidth then
         -- Change按钮被点击，执行相应代码
         -- TODO: 在这里添加Change按钮的代码逻辑
-        print("Change按钮被点击")
+        monitor.clear()
+        monitor.setCursorPos(1, 1)
+        monitor.write("Now you can change music")
+        sleep(2)
+        -- 返回原始界面
+        monitor.clear()
+        monitor.setCursorPos(changeButtonX, buttonY)
+        monitor.write("[ Change ]")
+        monitor.setCursorPos(exitButtonX, buttonY)
+        monitor.write("[ Exit ]")
     elseif yPos == buttonY and xPos >= exitButtonX and xPos < exitButtonX + buttonWidth then
         -- Exit按钮被点击，执行相应代码
         -- TODO: 在这里添加Exit按钮的代码逻辑
-        print("Exit按钮被点击")
+        monitor.clear()
+        monitor.setCursorPos(1, 1)
+        monitor.write("Quiting.........")
+        sleep(2)
         break  -- 退出程序
     end
 end
